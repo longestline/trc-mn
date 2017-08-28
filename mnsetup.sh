a0ddef7945b667eaf35f4f296a02acd4c62bdf57
longestline <longestline@users.noreply.github.com>
2017-08-28 13:45:52 -0500

    Simplified config file
    Removed unnecessary config values

--- mnsetup.sh
+++ mnsetup.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
 
-MY_IP=`ip route get 1 | awk '{print $NF;exit}'`
-RPCP=$(date +%s | sha256sum | base64 | head -c 32 ;)
-mkdir -p ~/.terracoincore/
-
-
 cat > ~/.terracoincore/terracoin.conf <<EOF
 rpcuser=trc
 rpcpassword=$RPCP
 rpcallowip=127.0.0.1
 rpcport=18320
 port=18321
-externalip=$MY_IP
 server=1
 listen=1
 daemon=1
 logtimestamps=1
-bind=$MY_IP:18321
-maxconnections=500
+maxconnections=256
 masternode=1
-masternodeaddr=$MY_IP:18321
 masternodeprivkey=XXXXXXXXX
-stake=0
-staking=0
 EOF
